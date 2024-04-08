c5fb888af30321d150b95b83cbaf72b7e12acba7
Jimmie Högklint <hogklint@gmail.com>
2024-04-08 18:45:45 +0200

    Autoinstall plugin manager

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,3 +1,14 @@
+local plugpath = vim.fn.stdpath("data") .. "/site/autoload/plug.vim"
+if not (vim.uv or vim.loop).fs_stat(plugpath) then
+  vim.fn.system({
+    "curl",
+    "-fLo",
+    plugpath,
+    "--create-dirs",
+    "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim",
+  })
+end
+
 local Plug = vim.fn["plug#"]
 
 vim.call("plug#begin")
@@ -75,15 +86,15 @@ vim.keymap.set("n", "<leader>o", ":QFix<CR>")
 
 -- Smartcolumn (colorcolumn)
 require("smartcolumn").setup(
-	{ colorcolumn = "120", disabled_filetypes = { "help", "text" } }
+  { colorcolumn = "120", disabled_filetypes = { "help", "text" } }
 )
 
 
 -- Python Black
 -- The plugin uses nvim's python env (see vim.g.python3_host_prog)
 vim.api.nvim_create_autocmd("BufWritePre", {
-	pattern = "*.py",
-	command = "call BlackSync()",
+  pattern = "*.py",
+  command = "call BlackSync()",
 })
 
 
@@ -91,59 +102,59 @@ vim.api.nvim_create_autocmd("BufWritePre", {
 -- nvim-cmp
 --
 local has_words_before = function()
-	unpack = unpack or table.unpack
-	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+  unpack = unpack or table.unpack
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
 
 local feedkey = function(key, mode)
-	vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
+  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
 end
 local cmp = require("cmp")
 cmp.setup({
-	snippet = {
-		expand = function(args)
-			vim.fn["vsnip#anonymous"](args.body)
-		end,
-	},
-	window = {
-		completion = cmp.config.window.bordered(),
-		documentation = cmp.config.window.bordered(),
-	},
-	mapping = cmp.mapping.preset.insert({
-		-- The <Tab> and <S-Tab> mapping functions mirror the behaviour of Super-Tab i.e. auto-complete when tabbing instead
-		-- of just moving in the list and then auto-complete with <CR>
-		["<Tab>"] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_next_item()
-			elseif vim.fn["vsnip#available"](1) == 1 then
-				feedkey("<Plug>(vsnip-expand-or-jump)", "")
-			elseif has_words_before() then
-				cmp.complete()
-			else
-				fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
-			end
-		end, { "i", "s" }),
+  snippet = {
+    expand = function(args)
+      vim.fn["vsnip#anonymous"](args.body)
+    end,
+  },
+  window = {
+    completion = cmp.config.window.bordered(),
+    documentation = cmp.config.window.bordered(),
+  },
+  mapping = cmp.mapping.preset.insert({
+    -- The <Tab> and <S-Tab> mapping functions mirror the behaviour of Super-Tab i.e. auto-complete when tabbing instead
+    -- of just moving in the list and then auto-complete with <CR>
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif vim.fn["vsnip#available"](1) == 1 then
+        feedkey("<Plug>(vsnip-expand-or-jump)", "")
+      elseif has_words_before() then
+        cmp.complete()
+      else
+        fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
+      end
+    end, { "i", "s" }),
 
-		["<S-Tab>"] = cmp.mapping(function()
-			if cmp.visible() then
-				cmp.select_prev_item()
-			elseif vim.fn["vsnip#jumpable"](-1) == 1 then
-				feedkey("<Plug>(vsnip-jump-prev)", "")
-			end
-		end, { "i", "s" }),
-		--["<C-Space>"] = cmp.mapping.complete(),
-		--["<C-e>"] = cmp.mapping.abort(),
-		["<CR>"] = cmp.mapping.confirm({ select = true }),
-		["<C-j>"] = cmp.mapping.confirm({ select = true }),
-	}),
-	sources = cmp.config.sources({
-		{ name = "nvim_lsp" },
-		{ name = "vsnip" },
-	}, {
-		{ name = "buffer" },
-	}),
-	preselect = cmp.PreselectMode.None,
+    ["<S-Tab>"] = cmp.mapping(function()
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif vim.fn["vsnip#jumpable"](-1) == 1 then
+        feedkey("<Plug>(vsnip-jump-prev)", "")
+      end
+    end, { "i", "s" }),
+    --["<C-Space>"] = cmp.mapping.complete(),
+    --["<C-e>"] = cmp.mapping.abort(),
+    ["<CR>"] = cmp.mapping.confirm({ select = true }),
+    ["<C-j>"] = cmp.mapping.confirm({ select = true }),
+  }),
+  sources = cmp.config.sources({
+    { name = "nvim_lsp" },
+    { name = "vsnip" },
+  }, {
+    { name = "buffer" },
+  }),
+  preselect = cmp.PreselectMode.None,
 })
 
 
