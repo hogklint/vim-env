06692cf54189d79e68e2b3d28157137dc08fe263
Jimmie Högklint <jimmie.hogklint@timeedit.com>
2024-06-07 20:08:18 +0200

    Add border to LSP hover action

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -64,6 +64,12 @@ vim.keymap.set("n", "<F5>", ":ClangdSwitchSourceHeader<CR>")
 --
 fts = { lua = "*.lua", go = "*.go", json = "*.json" }
 
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
+  vim.lsp.handlers.hover, {
+    border = "rounded"
+  }
+)
+
 vim.api.nvim_create_autocmd("LspAttach", {
   group = vim.api.nvim_create_augroup("UserLspConfig", {}),
   callback = function(ev)
