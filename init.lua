3ebf1403665815ebf4af5eb0ef1e003f4595c73b
Jimmie Högklint <hogklint@gmail.com>
2025-05-09 17:47:49 +0200

    Clean init.lua
    Had configs in vim files at one point. Since they are gone this is not
    needed.

--- init.lua
+++ init.lua
@@ -1,20 +1,5 @@
-local conf_files = {
-  "settings.lua",
-  "keymaps.lua",
-  "plugins.lua",
-  "lsp.lua",
-  "statusline.lua",
-}
-
-local viml_conf_dir = vim.fn.stdpath("config") .. "/viml_conf"
-for _, file_name in ipairs(conf_files) do
-  if vim.endswith(file_name, 'vim') then
-    local path = string.format("%s/%s", viml_conf_dir, file_name)
-    local source_cmd = "source " .. path
-    vim.cmd(source_cmd)
-  else
-    local module_name, _ = string.gsub(file_name, "%.lua", "")
-    package.loaded[module_name] = nil
-    require(module_name)
-  end
-end
+require("settings")
+require("keymaps")
+require("plugins")
+require("lsp")
+require("statusline")
