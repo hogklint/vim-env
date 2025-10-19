ec00486faa017eef607db353c49fa50de06b1468
Jimmie Högklint <hogklint@gmail.com>
2025-10-19 21:35:42 +0200

    fixup! Cmp plugins

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,10 +1,20 @@
 return {
-  -- nvim-cmp recommended setup
-  'hrsh7th/cmp-nvim-lsp'
-  'hrsh7th/cmp-buffer'
-  'hrsh7th/cmp-path'
-  'hrsh7th/cmp-cmdline'
-  'hrsh7th/nvim-cmp'
+  {
+    "hrsh7th/nvim-cmp",
+    version = false, -- last release is way too old
+    event = "InsertEnter",
+    dependencies = {
+      -- nvim-cmp recommended setup
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      'hrsh7th/cmp-cmdline',
+      "hrsh7th/cmp-path",
+    },
+    opts = function()
+      -- Register nvim-cmp lsp capabilities
+      vim.lsp.config("*", { capabilities = require("cmp_nvim_lsp").default_capabilities() })
+    end
+  },
 
   -- TODO: Optionally add intergration with snip-plugin
 }
