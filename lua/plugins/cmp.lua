65e025f8cae993ef1bf88b6f2dc3f60f94a8f3df
Jimmie Högklint <hogklint@gmail.com>
2025-10-19 20:41:44 +0200

    Cmp plugins

---
+++ lua/plugins/cmp.lua
@@ -0,0 +1,10 @@
+return {
+  -- nvim-cmp recommended setup
+  'hrsh7th/cmp-nvim-lsp'
+  'hrsh7th/cmp-buffer'
+  'hrsh7th/cmp-path'
+  'hrsh7th/cmp-cmdline'
+  'hrsh7th/nvim-cmp'
+
+  -- TODO: Optionally add intergration with snip-plugin
+}
