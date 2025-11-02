3e6ef72e28156ccee5ceb0ddea869a258a6a3be9
Jimmie Högklint <hogklint@gmail.com>
2025-11-02 20:51:59 +0100

    Some cleanup

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,38 +1,29 @@
 return {
-  "bergercookie/vim-debugstring",
-  --"hogklint/QFixToggle",
-  --"tpope/vim-fugitive",
   --"mbbill/undotree",
-  --"chrisbra/vim-diff-enhanced",
-  --"MattesGroeger/vim-bookmarks",
-  --"airblade/vim-rooter",
   --"godlygeek/tabular",
+  --"MattesGroeger/vim-bookmarks",
   --"hashivim/vim-terraform",
-  --"github/copilot.vim",
   --"NoahTheDuke/vim-just",
-
-  -- Color scheme
-  --"rebelot/kanagawa.nvim",
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-
+  --"chrisbra/vim-diff-enhanced",
   -- Autocomplete nvim config
   -- replace with lazydev.vim
   --"folke/neodev.nvim",
-
-  -- Enable repeat of vim-debugstring by pressing period
-  --"tpope/vim-repeat",
-
   -- Color column when max width is reached
   --Plug("m4xshen/smartcolumn.nvim",
-
   -- JSON and YAML schemas
   --"b0o/schemastore.nvim",
   --"towolf/vim-helm",
 
-  -- LSP and auto complete stuff
-  -- Insert auto complete with snipet plugin
-  --'hrsh7th/cmp-vsnip',
-  --'hrsh7th/vim-vsnip',
-  -- Show function signatures as your call them
-  --"ray-x/lsp_signature.nvim",
+  --"hogklint/QFixToggle",
+  --"tpope/vim-fugitive",
+  --"airblade/vim-rooter",
+  --"github/copilot.vim",
+
+  "bergercookie/vim-debugstring",
+  -- Enable repeat of vim-debugstring by pressing period
+  --"tpope/vim-repeat",
+
+  -- Color scheme
+  --"rebelot/kanagawa.nvim",
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
 }
