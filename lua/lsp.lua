dd98c9ec86284664c06a72ea40bc974e6442edca
Jimmie Högklint <hogklint@gmail.com>
2025-11-05 21:33:48 +0100

    lsp keys

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -51,48 +51,68 @@ vim.keymap.set("n", "<F5>", ":ClangdSwitchSourceHeader<CR>")
 --  }
 --)
 
---vim.api.nvim_create_autocmd("LspAttach", {
---  group = vim.api.nvim_create_augroup("UserLspConfig", {}),
---  callback = function(ev)
---    -- Enable completion triggered by <c-x><c-o>
---    -- vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
---
---    -- Buffer local mappings.
---    -- See `:help vim.lsp.*` for documentation on any of the below functions
---    local opts = { buffer = ev.buf }
---    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { buffer = ev.buf, desc = "G"})
---    vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = ev.buf, desc = "asdf"})
---    vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = ev.buf, desc = "asdf" })
---    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { buffer = ev.buf, desc = "asdf" })
---    vim.keymap.set("n", "gc", vim.lsp.codelens.display, { buffer = ev.buf, desc = "asdf" })
---    -- vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, { buffer = ev.buf, desc = "asdf" })
---    -- vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, { buffer = ev.buf, desc = "asdf" })
---    -- vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, { buffer = ev.buf, desc = "asdf" })
---    -- vim.keymap.set("n", "<space>wl", function()
---    --   print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
---    -- end, { buffer = ev.buf, desc = "asdf" })
---    -- vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, { buffer = ev.buf, desc = "asdf" })
---    vim.keymap.set("n", "<leader>a", vim.lsp.buf.rename, { buffer = ev.buf, desc = "asdf" })
---    vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { buffer = ev.buf, desc = "asdf" })
---    vim.keymap.set("n", "gr", vim.lsp.buf.references, { buffer = ev.buf, desc = "asdf" })
---    vim.keymap.set("n", "<leader>g", function()
---      vim.lsp.buf.format { async = true }
---    end, { buffer = ev.buf, desc = "asdf" })
---
---    local client = vim.lsp.get_client_by_id(ev.data.client_id)
---    --patterns = {}
---    --for i, ft in ipairs(client.config.filetypes or {}) do
---    --  if fts[ft] ~= nil then
---    --    table.insert(patterns, fts[ft])
---    --  end
---    --end
---    --if client.server_capabilities.documentFormattingProvider and next(patterns) ~= nil then
---    --  vim.api.nvim_create_autocmd("BufWritePre", {
---    --    pattern = patterns,
---    --    callback = function()
---    --      vim.lsp.buf.format { async = false }
---    --    end
---    --  })
---    --end
---  end,
---})
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+  callback = function(ev)
+    -- Enable completion triggered by <c-x><c-o>
+    -- vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
+
+    -- Buffer local mappings.
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    local opts = { buffer = ev.buf }
+    --vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { buffer = ev.buf, desc = "G"})
+    --vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = ev.buf, desc = "asdf"})
+    --vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = ev.buf, desc = "asdf" })
+    --vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { buffer = ev.buf, desc = "asdf" })
+    --vim.keymap.set("n", "gc", vim.lsp.codelens.display, { buffer = ev.buf, desc = "asdf" })
+    -- vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, { buffer = ev.buf, desc = "asdf" })
+    -- vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, { buffer = ev.buf, desc = "asdf" })
+    -- vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, { buffer = ev.buf, desc = "asdf" })
+    -- vim.keymap.set("n", "<space>wl", function()
+    --   print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    -- end, { buffer = ev.buf, desc = "asdf" })
+    -- vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, { buffer = ev.buf, desc = "asdf" })
+    --vim.keymap.set("n", "<leader>a", vim.lsp.buf.rename, { buffer = ev.buf, desc = "asdf" })
+    --vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { buffer = ev.buf, desc = "asdf" })
+    --vim.keymap.set("n", "gr", vim.lsp.buf.references, { buffer = ev.buf, desc = "asdf" })
+    --vim.keymap.set("n", "<leader>g", function()
+    --  vim.lsp.buf.format { async = true }
+    --end, { buffer = ev.buf, desc = "asdf" })
+
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Goto Definition", buffer = ev.buf })
+    vim.keymap.set("n", "gr", vim.lsp.buf.references, { desc = "References", nowait = true })
+    vim.keymap.set("n", "gI", vim.lsp.buf.implementation, { desc = "Goto Implementation" })
+    vim.keymap.set("n", "gy", vim.lsp.buf.type_definition, { desc = "Goto T[y]pe Definition" })
+    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { desc = "Goto Declaration" })
+    vim.keymap.set("n", "K", function() return vim.lsp.buf.hover() end, { desc = "Hover" })
+    vim.keymap.set("n", "gK", function() return vim.lsp.buf.signature_help() end, { desc = "Signature Help" })
+    vim.keymap.set("i", "<c-k>", function() return vim.lsp.buf.signature_help() end, { desc = "Signature Help" })
+    vim.keymap.set({"n", "x"}, "<leader>ca", vim.lsp.buf.code_action, { desc = "Code Action" })
+    vim.keymap.set({"n", "x"}, "<leader>cc", vim.lsp.codelens.run, { desc = "Run Codelens" })
+    vim.keymap.set("n", "<leader>cC", vim.lsp.codelens.refresh, { desc = "Refresh & Display Codelens" })
+    vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename, { desc = "Rename" })
+    vim.keymap.set("n", "<leader>cA", vim.lsp.buf.code_action, { desc = "Source Action" })
+    --vim.keymap.set("n", "<leader>cl", function() Snacks.picker.lsp_config() end, { desc = "Lsp Info" })
+    --vim.keymap.set("n", "<leader>cR", function() Snacks.rename.rename_file() end, { desc = "Rename File", has = { "workspace/didRenameFiles", "workspace/willRenameFiles" }})
+    --vim.keymap.set("n", "]]", function() Snacks.words.jump(vim.v.count1) end, has = "documentHighlight", { desc = "Next Reference", enabled = function() return Snacks.words.is_enabled() end })
+    --vim.keymap.set("n", "[[", function() Snacks.words.jump(-vim.v.count1) end, has = "documentHighlight", { desc = "Prev Reference", enabled = function() return Snacks.words.is_enabled() end })
+    --vim.keymap.set("n", "<a-n>", function() Snacks.words.jump(vim.v.count1, true) end, has = "documentHighlight", { desc = "Next Reference", enabled = function() return Snacks.words.is_enabled() end })
+    --vim.keymap.set("n", "<a-p>", function() Snacks.words.jump(-vim.v.count1, true) end, has = "documentHighlight", { desc = "Prev Reference", enabled = function() return Snacks.words.is_enabled() end })
+
+    local client = vim.lsp.get_client_by_id(ev.data.client_id)
+    --patterns = {}
+    --for i, ft in ipairs(client.config.filetypes or {}) do
+    --  if fts[ft] ~= nil then
+    --    table.insert(patterns, fts[ft])
+    --  end
+    --end
+    --if client.server_capabilities.documentFormattingProvider and next(patterns) ~= nil then
+    --  vim.api.nvim_create_autocmd("BufWritePre", {
+    --    pattern = patterns,
+    --    callback = function()
+    --      vim.lsp.buf.format { async = false }
+    --    end
+    --  })
+    --end
+  end,
+})
