3ec1fc65d0f0461746aaef5050cb7a39013b549a
Jimmie Högklint <hogklint@gmail.com>
2025-11-18 14:30:22 +0100

    Remove copilot no tab map

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,23 +1,24 @@
 return {
-	{
-		"github/copilot.vim",
+  {
+    "github/copilot.vim",
 
-		-- Lazy-load on insert mode
-		--event = "InsertEnter",
+    -- Lazy-load on insert mode
+    --event = "InsertEnter",
 
-		config = function()
-			local map = vim.keymap.set
+    config = function()
+      local map = vim.keymap.set
 
-			map("i", "<C-l>", 'copilot#Accept("<CR>")', {
-				expr = true,
-				replace_keycodes = false,
-				silent = true,
-				noremap = true,
-			})
+      map("i", "<C-l>", 'copilot#Accept("<CR>")', {
+        expr = true,
+        replace_keycodes = false,
+        silent = true,
+        noremap = true,
+      })
+      vim.g.copilot_no_tab_map = true
 
-			--map("i", "<C-j>", "<Plug>(copilot-next)", { silent = true })
-			--map("i", "<C-k>", "<Plug>(copilot-previous)", { silent = true })
-			map("i", "<C-d>", "<Plug>(copilot-dismiss)", { silent = true })
-		end,
-	},
+      --map("i", "<C-j>", "<Plug>(copilot-next)", { silent = true })
+      --map("i", "<C-k>", "<Plug>(copilot-previous)", { silent = true })
+      map("i", "<C-d>", "<Plug>(copilot-dismiss)", { silent = true })
+    end,
+  },
 }
