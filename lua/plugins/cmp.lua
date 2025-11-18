76008f078f540aeb86c20e2d64bedd397eab560b
Jimmie Högklint <hogklint@gmail.com>
2025-11-18 14:30:35 +0100

    cmp keymaps

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,25 +1,72 @@
 return {
-	{
-		"hrsh7th/nvim-cmp",
-		version = false, -- last release is way too old
-		event = "InsertEnter",
-		dependencies = {
-			-- nvim-cmp recommended setup
-			"hrsh7th/cmp-nvim-lsp",
-			"hrsh7th/cmp-buffer",
-			"hrsh7th/cmp-cmdline",
-			"hrsh7th/cmp-path",
-		},
-		opts = function()
-			-- Register nvim-cmp lsp capabilities
-			vim.lsp.config("*", { capabilities = require("cmp_nvim_lsp").default_capabilities() })
-			return {
-				mapping = {
-					["<C-Space>"] = cmp.mapping.complete(), -- Replace with whatever you want
-				},
-			}
-		end,
-	},
+  {
+    "hrsh7th/nvim-cmp",
+    version = false, -- last release is way too old
+    event = "InsertEnter",
+    dependencies = {
+      -- nvim-cmp recommended setup
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-cmdline",
+      "hrsh7th/cmp-path",
+      'hrsh7th/cmp-vsnip',
+      'hrsh7th/vim-vsnip',
+    },
+    opts = function()
+      -- Register nvim-cmp lsp capabilities
+      vim.lsp.config("*", { capabilities = require("cmp_nvim_lsp").default_capabilities() })
+      local has_words_before = function()
+        unpack = unpack or table.unpack
+        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+      end
 
-	-- TODO: Optionally add intergration with snip-plugin
+      local feedkey = function(key, mode)
+        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
+      end
+      local cmp = require("cmp")
+      return {
+        window = {
+          completion = cmp.config.window.bordered(),
+          documentation = cmp.config.window.bordered(),
+        },
+        mapping = cmp.mapping.preset.insert({
+          -- The <Tab> and <S-Tab> mapping functions mirror the behaviour of Super-Tab i.e. auto-complete when tabbing instead
+          -- of just moving in the list and then auto-complete with <CR>
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif vim.fn["vsnip#available"](1) == 1 then
+              feedkey("<Plug>(vsnip-expand-or-jump)", "")
+            elseif has_words_before() then
+              cmp.complete()
+            else
+              fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
+            end
+          end, { "i", "s" }),
+
+          ["<S-Tab>"] = cmp.mapping(function()
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif vim.fn["vsnip#jumpable"](-1) == 1 then
+              feedkey("<Plug>(vsnip-jump-prev)", "")
+            end
+          end, { "i", "s" }),
+          --["<C-Space>"] = cmp.mapping.complete(),
+          --["<C-e>"] = cmp.mapping.abort(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+          ["<C-j>"] = cmp.mapping.confirm({ select = true }),
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "vsnip" },
+        }, {
+          { name = "buffer" },
+        }),
+        preselect = cmp.PreselectMode.None,
+      }
+    end,
+  },
+
+  -- TODO: Optionally add intergration with snip-plugin
 }
